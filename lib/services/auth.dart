e3f9fa1b68961c9ce4114033c33f4b3b85d22741
BUOmerBebo <b2477332@ben.edu>
2021-11-21 18:24:31 -0600

    screens

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,11 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   // sign in anon
   Future signInAnon() async {
     try {
-
-      UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+      UserCredential userCredential = await _auth.signInAnonymously();
       User? user = userCredential.user;
       return user;
     } catch(e) {
