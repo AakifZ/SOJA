88834ca1055340df8b767c31eaa04f42b1bd42e8
jonathancampos <b2366870@ben.edu>
2021-11-22 19:15:43 -0600

    updated - JayC

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:soja/screens/wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:soja/services/MyThemes.dart';
 import 'package:soja/services/auth.dart';
 
 import 'models/user.dart';
@@ -17,17 +18,24 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return StreamProvider<MyUser?>.value(
-      value: AuthService().user,
-      catchError: (User, MyUser) => null,
-      initialData: null,
-      child: MaterialApp(
-      home: Wrapper(),
-      ),
-    );
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      builder: (context, _) {
+        return StreamProvider<MyUser?>.value(
+          value: AuthService().user,
+          catchError: (User, MyUser) => null,
+          initialData: null,
+          child: MaterialApp(
+            themeMode: ThemeMode.system,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: Wrapper(),
+          ),
+        );
+      });
   }
 
 
 
-}
+
+
---
+++ lib/services/changeTheme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:soja/services/MyThemes.dart';
+
+
+class ChangeTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    
+    return Switch.adaptive(
+      value: themeProvider.isDarkMode,
+      onChanged: (value) {
+
+      },
+    );
+  }
+  
+}
