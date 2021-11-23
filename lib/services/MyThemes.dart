dd15f3f00f24ef724065f353841161f836a906de
jonathancampos <b2366870@ben.edu>
2021-11-22 23:05:29 -0600

    updated light/dark mode - JayC

--- lib/services/MyThemes.dart
+++ lib/services/MyThemes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+//Here is the color change effect class that allows the user to switch from light to dark mode.
 class ThemeProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.dark;
 
@@ -11,12 +11,14 @@ class ThemeProvider extends ChangeNotifier {
   }
 }
 class MyThemes {
+  //Dark mode method
   static final darkTheme = ThemeData(
     scaffoldBackgroundColor: Colors.grey.shade900,
     colorScheme: ColorScheme.dark(),
 
   );
 
+  //Lgith mode method
   static final lightTheme = ThemeData(
   scaffoldBackgroundColor: Colors.white,
     colorScheme: ColorScheme.light(),
