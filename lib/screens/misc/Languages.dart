9e2f3eefb5c5d58ccbc5a0fa336d7c72248fd375
AakifBen <aakifz333@gmail.com>
2021-11-30 08:49:36 -0600

    Added Langauges button

--- lib/screens/misc/Languages.dart
+++ lib/screens/misc/Languages.dart
@@ -1,58 +1,59 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-//
-//
-// class LanguagesPage extends StatelessWidget {
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Languages'),),
-//       body: Container(
-//         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
-//
-//         child: Column(
-//
-//             children: <Widget>[
-//
-//               //Button For English
-//
-//               ElevatedButton(onPressed: () {
-//                 Navigator.push(
-//                     context, MaterialPageRoute(builder: (context) => ()));
-//               }, child: Text("English", style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.w500,
-//                 color: Colors.grey[600],
-//               ))),
-//
-//               //Button For Spanish
-//
-//               ElevatedButton(onPressed: () {
-//                 Navigator.push(
-//                     context, MaterialPageRoute(builder: (context) => ()));
-//               }, child: Text("Spanish", style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.w500,
-//                 color: Colors.grey[600],
-//               ))),
-//
-//               //Button for French
-//
-//               ElevatedButton(onPressed: () {
-//                 Navigator.push(
-//                     context, MaterialPageRoute(builder: (context) => ()));
-//               }, child: Text("French", style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.w500,
-//                 color: Colors.grey[600],
-//               )))
-//
-//
-//             ]
-//         ),
-//       ),
-//     );
-//   }
-// }
+/*
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class LanguagesPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Languages'),),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
+
+        child: Column(
+
+            children: <Widget>[
+
+              //Button For English
+
+              ElevatedButton(onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => ()));
+              }, child: Text("English", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ))),
+
+              //Button For Spanish
+
+              ElevatedButton(onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => ()));
+              }, child: Text("Spanish", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ))),
+
+              //Button for French
+
+              ElevatedButton(onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => ()));
+              }, child: Text("French", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              )))
+
+
+            ]
+        ),
+      ),
+    );
+  }
+}*/
